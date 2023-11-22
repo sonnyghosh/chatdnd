adfc515e92e10dd48129ddc0d6d8a84671bb5eba
Jack Quimby <jack.quimby54@gmail.com>
2023-11-22 16:52:47 -0500

    openaip integration complete!

--- api/aiapi.py
+++ api/aiapi.py
@@ -1,11 +1,12 @@
-import openai
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 token = os.environ.get("OPENAI_API_KEY")
+client = OpenAI(api_key=token)
 
-def generateStoryResponse(prompt, context):
+def generateStoryResponse(prompt, context, max_len=450):
     messages = []
     messages.append({"role": "system", "content": "You are the dungeon master of the wonderful game of chatDND. You provide completions to the storyline with user options to pick what to do next. The storyline should be interesting and challenging."})
     messages.append({"role": "system", "content": context})
@@ -14,11 +15,40 @@ def generateStoryResponse(prompt, context):
     question['content'] = prompt
     messages.append(question)
     
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
-
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages, max_tokens=mex_len)
     try:
-        answer = response['choices'][0]['message']['content'].replace('\n','<br>') # return as html
-    except:
+        answer = response.choices[0].message.content.replace('\n','<br>') # return as html
+    except Exception as e:
+        print(e)
         answer = 'cannot get response from API'
 
     return answer
+
+'''
+Example of a chat completion object
+ChatCompletion(
+    id='chatcmpl-8Np6lx28vkijp95wIkelV54neBrlg', 
+    choices=[
+        Choice(
+            finish_reason='length', 
+            index=0, 
+            message=ChatCompletionMessage(
+                content='You decide to continue exploring the cavern, leaving the rare gem behind for now. As you venture deeper into the darkness, you notice a faint glow up ahead. Curiosity gets the better of you, and you cautiously move towards the source of the light.\n\nAs you draw closer, you discover a chamber filled with glowing mushrooms. The soft bioluminescent light they emit illuminates the area, revealing a hidden pathway leading deeper into the depths of the cavern. However, you also spot a cluster of', 
+                role='assistant', 
+                function_call=None, 
+                tool_calls=None
+            )
+        )
+    ],
+    created=1700688695, 
+    model='gpt-3.5-turbo-0613', 
+    object='chat.completion', 
+    system_fingerprint=None, 
+    usage=CompletionUsage(
+        completion_tokens=100, 
+        prompt_tokens=87, 
+        total_tokens=187
+    )
+)
+
+'''
