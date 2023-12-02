be103e8d8a3114c05fe5be54c5e1745d75798fb3
Sonny Ghosh <ghoshsonny10@gmail.com>
2023-12-02 02:44:41 -0500

    added testing for partycontainer

--- dnd-client/src/components/PartyContainer.jsx
+++ dnd-client/src/components/PartyContainer.jsx
@@ -1,24 +1,36 @@
 export const PartyContainer = (PartyMembers) => {
     const { partyList } = PartyMembers;
+    console.log( partyList ); 
     return (
-        <div className="border border-red-950 flex flex-col flex-auto overflow-y-auto">
-            {partyList.map(member => (
-                <div key={member.id} className="flex flex-auto flex-row">
-                    <div className="flex flex-auto flex-col">
-                        <img src={`data:image/png;base64,${ member.profile_picture }`} className="h-12 w-12"/>   
-                    </div>
-                    <div className="flex flex-auto flex-col">
-                        <p> { member.name } </p>
-                        <p>Level: { member.level }</p>
-                        <p>Class: { member.gameClass }</p>
-                    </div>
-                    <div className="flex flex-auto flex-col">
-                        <p>HP: { member.HP }</p>
-                        <p>MP: { member.MP }</p>
-                        <p>STA: { memver.STA }</p>
-                    </div>
-                </div>
+        <div className="border border-red-950 overflow-y-auto text-white font-custom bg-slate-800 h-80 w-96 rounded-md">
+            <div className="grid grid-cols-3">
+                {partyList.map(member => (
+                    <div key={member.id} className="">
+                    </div> 
             ))}
+            </div>
         </div>
-    )
-}
+            
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
